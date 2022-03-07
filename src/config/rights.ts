ba9039f9ebcf314795a90e08d6529d1235423252
Aleksandr Panin <Aleksandr_Panin@epam.com>
2022-03-07 17:24:31 +0500

    [master] fixed build

--- src/config/rights.ts
+++
@@ -1,8 +0,0 @@
-import { ROLES } from ".prisma/client"
-
-const rights: Map<string, string[]> = new Map()
-rights.set(ROLES.ADMIN, ['manageItems'])
-rights.set(ROLES.EMPLOYEE, ['manageItems'])
-rights.set(ROLES.MANAGER, ['manageItems'])
-
-export default rights
