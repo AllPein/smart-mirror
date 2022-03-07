74be873681e6ea248fd96a1ba7174a81713b007c
Aleksandr Panin <Aleksandr_Panin@epam.com>
2022-03-07 17:24:18 +0500

    [master] fixed build

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,25 +5,14 @@ const prisma = new PrismaClient();
 const createUser = async () => {
   await prisma.user.create({
     data: {
-      biometricData: ['super_data', 'super_data_2']    
-    }
-  });
-};
-
-const createSettings = async () => {
-  await prisma.settings.create({
-    data: {
-      userId: '764e473f-9ef1-4246-bc78-ce0d5aa798a5',
-      weather: true,
-      currency: true,
-      news: true
+      embeddings: [0.13, 0.38, 0.54, 0.76],
+      name: 'Александр'    
     }
   });
 };
 
 const main = async () => {
-  // await createUser()
-  await createSettings()
+  await createUser()
 }
 
 main().then(() => {
